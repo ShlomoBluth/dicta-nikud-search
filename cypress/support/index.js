797c35aef14be2e4fc0714f3e3083f9c850a2da0
Shlomo Bluth <lomibluth@gmail.com>
2021-08-24 16:03:39 +0300

    add commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,17 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '../../dicta-shared/index.js'
 
-import addContext from 'mochawesome/addContext'
-
-Cypress.on("test:after:run", (test, runnable) => {
-    
-    let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
-
-    addContext({ test }, videoUrl)
-});
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
